93646ed6845d54e25c3f2daa510abf1c2a452d4a
Gavhart <113486288+Gavhart@users.noreply.github.com>
2023-05-18 11:24:50 -0600

    updated everything

--- Week 2/week02/scripts/task2.js
+++ Week 2/week02/scripts/task2.js
@@ -3,7 +3,7 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
-let myName = "Zach Southwick";
+let myName = "Gavin Hart";
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
 document.querySelector('#name').innerHTML = myName;
@@ -15,7 +15,7 @@ let currentYear = 2023;
 document.querySelector('#year').innerHTML = currentYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-let picLocation = 'images/profile.jpg';
+let picLocation = 'images/profile.jpeg';
 
 // Step 6: copy your image into the "images" folder
 
@@ -27,13 +27,13 @@ document.querySelector('img').src = picLocation;
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-let favFoods = ["Sushi", "Pizza", "Tacos"];
+let favFoods = ["Sushi", "BBQ", "Tacos"];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
 document.querySelector('#food').innerHTML = favFoods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-let newFav = "Chicken Parm";
+let newFav = "BBQ";
 
 // Step 4: add the variable holding another favorite food to the favorite food array
 favFoods.push(newFav);
